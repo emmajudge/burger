5e20f2d8ef106c001fcedd8cfcf1246e468367fd
emmajudge <emmalyn.judge@u.northwestern.edu>
2019-09-26 12:40:11 -0500

    updated handlebars, connection, and server

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 var express = require("express");
+var methodOverride=require("method-override");
 
 var PORT = process.env.PORT || 3000;
 
 var app = express();
 
 // Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static("public"));
+app.use(express.static(__dirname+ "/public"));
 
 // Parse application body as JSON
 app.use(express.urlencoded({ extended: true }));
@@ -14,6 +15,7 @@ app.use(express.json());
 // Set Handlebars.
 var exphbs = require("express-handlebars");
 
+app.use(methodOverride("_method"));
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
